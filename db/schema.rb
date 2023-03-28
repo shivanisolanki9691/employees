c9cc6ce36396ecaa5d401b25555fdd0c296871e7
shivanisolanki <shivanisolanki9691@gmail.com>
2023-03-28 14:55:31 +0530

    deploy

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_09_053116) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -56,7 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_053116) do
   create_table "subjects", force: :cascade do |t|
     t.string "name"
     t.string "sub_type"
-    t.integer "student_id", null: false
+    t.bigint "student_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["student_id"], name: "index_subjects_on_student_id"
